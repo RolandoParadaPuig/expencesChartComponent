2138a8b4e9650a45d0e7b3a42e2fd8c32e69efe0
Rolando Parada <rolandoparadapuig@gmail.com>
2022-05-31 23:45:40 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./css/App.css";
 import { ChartContainer } from "./components/chartContainer/ChartContainer";
 
 function App() {
