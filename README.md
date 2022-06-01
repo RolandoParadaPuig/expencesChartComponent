59177a0c391d182c2853f9e21287cc746c0ec328
Rolando Parada <rolandoparadapuig@gmail.com>
2022-05-31 23:26:02 -0400

    firebase deploy

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # expencesChartComponent
+
 Expences Chart Component on Reactjs
 
 This is my https://www.frontendmentor.io/ challenge proyect
 This is an Expences Chart Component wich have dinamic data called from a JSON file.
+imagen de fondo gracias a:
+
+<!-- <a href='https://www.freepik.es/vectores/formas-diseno'>Vector de formas de diseno creado por freepik - www.freepik.es</a> -->
+
+web deployed on:
+https://rolando-portfolio.web.app/
--- src/components/chartComponent/Bars.jsx
+++ src/components/chartComponent/Bars.jsx
@@ -1,6 +1,6 @@
 import { Col } from "antd";
 import React from "react";
-
+import moment from "moment";
 export const Bars = (props) => {
   const divisor = 7;
   const data = props.data;
@@ -8,28 +8,37 @@ export const Bars = (props) => {
   for (let i = 0; i < data.length; i++) {
     if (maxValue < data[i].amount) maxValue = data[i].amount;
   }
-  const onClick = (e) => {
+
+  const onEnter = (e) => {
     const element = e.target.classList;
     element.toggle("active");
-    element.toggle("no-active");
     const amountElem = document.getElementById(`bar-${e.target.id}`);
     amountElem.classList.toggle("active-amount");
-    console.log(element);
+  };
+  const onLeave = (e) => {
+    const element = e.target.classList;
+    element.toggle("active");
+    const amountElem = document.getElementById(`bar-${e.target.id}`);
+    amountElem.classList.toggle("active-amount");
   };
   return data.map((e) => {
+    const dayStr =
+      moment()._locale._weekdaysShort[moment().day() == 7 ? 0 : moment().day()];
+
     const index = data.indexOf(e);
     return (
-      <Col className="chart--bar">
+      <Col className="chart--bar" key={index}>
         <div className="bar--amount active-amount" id={`bar-${index}`}>
           {data[index].amount}
         </div>
         <div
           id={`${index}`}
-          className={`bar no-active ${
-            data[index].amount == maxValue ? "bar--blue" : "bar--red"
+          className={`bar ${
+            data[index].day == dayStr.toLowerCase() ? "bar--blue" : "bar--red"
           }`}
           style={{ height: `${data[index].amount / divisor}rem` }}
-          onClick={onClick}
+          onMouseEnter={onEnter}
+          onMouseLeave={onLeave}
         />
         {data[index].day}
       </Col>
