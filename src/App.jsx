1cc5df4f5c33a7ab7fd33362bd937be93c74efbe
Rolando Parada <rolandoparadapuig@gmail.com>
2022-06-13 13:38:13 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path={"/*"} element={<ChartContainer />} />
+          <Route path={"/expences-chart"} element={<ChartContainer />} />
         </Routes>
       </Router>
     </>
